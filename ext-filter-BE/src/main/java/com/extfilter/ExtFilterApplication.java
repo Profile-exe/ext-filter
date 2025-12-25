3d7e9bf64c64514548887466211b12dd6a0e89a7
Profile-exe <kang67572346@gmail.com>
2025-12-25 16:11:25 +0900

    chore: 프로젝트 초기 설정
    - FE: Vite + React(javascript)
    - BE: Springboot(java)

---
+++ ext-filter-BE/src/main/java/com/extfilter/ExtFilterApplication.java
@@ -0,0 +1,13 @@
+package com.extfilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtFilterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExtFilterApplication.class, args);
+    }
+
+}
