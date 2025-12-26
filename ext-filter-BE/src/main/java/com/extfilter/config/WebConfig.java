87e73c1c26ea83d2991a98d840505db4928bbce6
Profile-exe <kang67572346@gmail.com>
2025-12-26 18:03:56 +0900

    [BE] chore: FE 배포 주소 CORS 허용

--- ext-filter-BE/src/main/java/com/extfilter/config/WebConfig.java
+++ ext-filter-BE/src/main/java/com/extfilter/config/WebConfig.java
@@ -12,7 +12,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
-                        "http://localhost:5173"
+                        "http://localhost:5173",
+                        "https://ext-filter.vercel.app"
                 )
                 .allowedMethods(
                         HttpMethod.GET.name(),
